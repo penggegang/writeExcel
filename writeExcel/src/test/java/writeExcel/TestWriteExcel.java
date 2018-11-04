e818fcafcf6650687f2d724474961557cc7938ca
penggegang <penggegang@windows10.microdone.cn>
2018-11-04 17:28:33 +0800

    v0.8

---
+++ writeExcel/src/test/java/writeExcel/TestWriteExcel.java
@@ -0,0 +1,35 @@
+package writeExcel;
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestWriteExcel {
+   
+	@Before
+	 public void setUp() throws Exception {
+	  System.out.println("≤‚ ‘ø™ º");
+	 }
+
+	 @After
+	 public void tearDown() throws Exception {
+	  System.out.println("≤‚ ‘Ω· ¯...");
+	 }
+	 
+	@Test
+	public void test001() {
+		assertEquals(1,1);
+	}
+	
+	@Test
+	public void test002() {
+		assertEquals(1,1);
+	}
+	
+	@Test
+	public void test003() {
+		assertEquals(1,3);
+	}
+}
